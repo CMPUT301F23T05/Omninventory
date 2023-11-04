964abceb450bd6a4c575bf96d346d065b81a6a27
kevin-was-here <code --wait>
2023-11-04 02:56:14 -0600

    Updated and completed skeleton test cases

--- Project/app/src/androidTest/java/com/example/omninventory/TestProfileActivity.java
+++ Project/app/src/androidTest/java/com/example/omninventory/TestProfileActivity.java
@@ -1,11 +1,13 @@
 package com.example.omninventory;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class TestProfileActivity {
 
     @Test
---
+++ Project/app/src/androidTest/java/com/example/omninventory/TestTagActivity.java
@@ -0,0 +1,34 @@
+package com.example.omninventory;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class TestTagActivity {
+    //Note that editing adding and editing tags are handled in
+    // testAddItemActivity and testEditItemActivity.
+    TestItems testItems;
+
+    @Test
+    public void testAddTag(){
+        //From inventory screen select tags button
+        //Select add tag
+        //Enter in dialog box new tag name
+        //click add button
+        //validate new tag added
+    }
+
+    @Test
+    public void testDeleteTag(){
+        testItems.generateTestItems(); //Test items generated should have tags attached
+        //From inventory screen select tags button
+        //Select a tag and select the delete button
+        //Click on delete
+        //Validate the tag is deleted
+        //validate items associated with the tag also has the tag removed.
+    }
+}
