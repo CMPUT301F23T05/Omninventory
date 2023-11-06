0cb8bbb3feeca35f888177148edad3692aad2224
Shem <castorsh3m@gmail.com>
2023-11-05 21:27:16 -0700

    tweaks to ValueTextWatcher behaviour

--- Project/app/src/main/java/com/example/omninventory/ValueTextWatcher.java
+++ Project/app/src/main/java/com/example/omninventory/ValueTextWatcher.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import android.widget.EditText;
 
 import java.lang.ref.WeakReference;
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,6 +21,9 @@ public class ValueTextWatcher implements TextWatcher {
     public ValueTextWatcher(EditText editText) {
         // initialize with reference to EditText to watch
         editTextWeakReference = new WeakReference<EditText>(editText);
+
+        // make an empty Editable to mock an empty EditText and set EditText to a default value
+        this.afterTextChanged(Editable.Factory.getInstance().newEditable(""));
     }
 
     @Override
@@ -39,18 +44,15 @@ public class ValueTextWatcher implements TextWatcher {
         if (editText == null) return;
 
         String s = editable.toString();
-        if (s.isEmpty()) return;
+        Log.d("ValueTextWatcher", String.format("called with Editable: '%s'", s));
 
-        Log.d("ValueTextWatcher", s);
+        // get rid of non digits
+        String digits = s.replaceAll("[\\D]", "");
 
-        editText.removeTextChangedListener(this);
-
-        String digits = s.replaceAll("[\\D]", ""); // get rid of non digits
-
-        // take all digits as cents
+        // consider all digits entered to be the number of cents
         Long centsNum;
         try {
-            if (digits == "") {
+            if (digits.equals("")) {
                 centsNum = 0L;
             } else {
                 centsNum = Long.parseLong(digits);
@@ -60,15 +62,19 @@ public class ValueTextWatcher implements TextWatcher {
             centsNum = Long.MAX_VALUE;
         }
 
-        // separate into dollars and cents
-        Long sepDollarsNum = centsNum / 100;
-        Long sepCentsNum = centsNum - 100 * sepDollarsNum;
+        // separate value into dollars and cents
+        long sepDollarsNum = centsNum / 100;
+        long sepCentsNum = centsNum - 100 * sepDollarsNum;
 
-        // replace contents with new formatted string
-        String formatted = String.format("%d.%02d", sepDollarsNum, sepCentsNum);
+        // using Locale.CANADA here makes a lot of assumptions, however dealing with different
+        // currency formats is not within the scope of this project
+        Log.d("ValueEditText", String.format("Formatting values: %d, %d", sepDollarsNum, sepCentsNum));
+        String formatted = String.format(Locale.CANADA, "$%d.%02d", sepDollarsNum, sepCentsNum);
+
+        // replace contents of EditText with new formatted string
+        editText.removeTextChangedListener(this);
         editText.setText(formatted);
         editText.setSelection(formatted.length());
-
         editText.addTextChangedListener(this);
     }
 }
