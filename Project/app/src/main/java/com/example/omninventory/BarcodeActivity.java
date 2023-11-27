a1ff162d647cce962dc3b3a7620fcbc2e4b4d11d
agu12345 <agu1@ualberta.ca>
2023-11-27 16:29:32 -0700

    Added camera to scan barcode(haven't tested if it actually scans barcode)

---
+++ Project/app/src/main/java/com/example/omninventory/BarcodeActivity.java
@@ -0,0 +1,73 @@
+package com.example.omninventory;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+
+/**
+ * Activity for scanning barcodes to specify an item description using the associated product
+ * information.
+ *
+ * @author Aron
+ */
+public class BarcodeActivity extends AppCompatActivity {
+
+    private CodeScanner mCodeScanner;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.barcode_scanner);
+        CodeScannerView scannerView = findViewById(R.id.barcode_scanner);
+        Button backButton = findViewById(R.id.barcode_back_button);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(BarcodeActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setResult(RESULT_CANCELED);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+}
