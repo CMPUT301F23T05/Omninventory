29de8b176d74b069f1ccabae3f24d6d6040c106c
kevin-was-here <code --wait>
2023-12-02 19:16:46 -0700

    Add tag test no working still

--- Project/app/src/androidTest/java/com/example/omninventory/TestAddItemActivity.java
+++ Project/app/src/androidTest/java/com/example/omninventory/TestAddItemActivity.java
@@ -7,12 +7,14 @@ import static androidx.test.espresso.action.ViewActions.longClick;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.action.ViewActions.swipeUp;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.Matchers.allOf;
 import static java.lang.Thread.sleep;
 
@@ -156,10 +158,69 @@ public class TestAddItemActivity {
     @Test
     public void testAddItemWithTag(){
         //Start on inventory screen, click on the add button
-        //in the add item screen, fill in the minimum required information for an item
-        //add tags to the item
-        //add the item
-        //validate from the inventory screen that the item has tags
+
+        // start on the inventory screen click on the add button
+        onView(allOf(withId(R.id.add_item_button), isDisplayed()))
+                .perform(click());
+
+        //in the add item screen fill in the minimum required information for an item
+        onView(withId(R.id.item_name_edittext)).perform(ViewActions.
+                typeText("AddItemTest"));
+        onView(withId(R.id.item_description_edittext)).perform(ViewActions.
+                typeText("AddItemTest description test"));
+
+        //add tag to item
+        onView(withId(R.id.item_tags_button))
+                .perform(scrollTo())
+                .check(matches(isDisplayed()));
+
+        onView(withId(R.id.item_tags_button)).perform(click());
+
+        onView(withText("important"))
+                .perform(scrollTo())
+                .check(matches(isDisplayed()))
+                .perform(click());
+
+        onView(withId(R.id.confirm_tags_button)).perform(click());
+
+        onView(withId(R.id.save_button)).perform(click());
+
+        onView(withText("AddItemTest"))
+                .perform(scrollTo())
+                .check(matches(isDisplayed()));
+
+        //validate in the inventory screen that the item was added
+        //may crash if the database is not reset before hand.
+        onView(withText("AddItemTest description test")).check(matches(isDisplayed()));
+
+        onView(withText("AddItemTest")).perform(click());
+
+        try {
+            Thread.sleep(2000); // Sleep for 1 second
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return;
+        }
+
+        // Scroll to the TextView that should contain the tag
+        onView(withId(R.id.act_details_view)) // ScrollView ID
+                .perform(scrollTo(), click());
+
+        // Now check if the TextView contains the expected tag
+        onView(withId(R.id.item_tags_text))
+                .check(matches(withText(containsString("#important"))));
+
+
+        try {
+            Thread.sleep(2000); // Sleep for 1 second
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return;
+        }
+
+        onView(withId(R.id.back_button)).perform(click());
+
+        cleanup();
     }
 
     @Test
