ceee310f37eebe3f60e7b4e936958c4bcd159f60
Shem <castorsh3m@gmail.com>
2023-12-04 11:34:24 -0700

    unit tests for Utils, ItemValue

---
+++ Project/app/src/test/java/com/example/omninventory/ItemValueUnitTest.java
@@ -0,0 +1,24 @@
+package com.example.omninventory;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ItemValueUnitTest {
+
+    @Test
+    public void test(){
+        ItemValue tester = new ItemValue("4500");
+
+        // Test conversion
+        assertEquals("$45.00", tester.toString());
+        assertEquals(4500L, tester.toPrimitiveLong());
+
+        // Test different formats
+        assertEquals("$45.01", ItemValue.makeValidString("4501"));
+        assertEquals("$45.01", ItemValue.makeValidString("$45.01"));
+        assertEquals("$45.01", ItemValue.makeValidString("4---=-/.501=-"));
+    }
+
+}
